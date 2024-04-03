ff1d939e3cbbf1e6ff28d2f27a3e6e1bf6017613
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-04-02 20:35:54 -0500

    Date data type and it's conversion

--- built_unit_converter.py
+++ built_unit_converter.py
@@ -1,3 +1,8 @@
+# Display today's date
 from datetime import date
-
 print(date.today())
+
+# Build a unit converter
+parsecs = 11
+lightyear = parsecs * 3.26
+print(str(parsecs) + " parsec is " + str(lightyear) + " lightyears")
