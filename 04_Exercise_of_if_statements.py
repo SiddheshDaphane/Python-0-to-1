9ddf6b882a4174a8d85d018aa0d4bc4853138cf4
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-04-03 01:46:27 -0500

    if statement exercise

--- 04_Exercise_of_if_statements.py
+++ 04_Exercise_of_if_statements.py
@@ -4,3 +4,12 @@ if object_size > 5:
     print("We need to keep an eye on this object")
 else:
     print("object poses no threat")
+
+
+# 2)
+object_size = 10
+proximity = 9000
+if object_size > 5 and proximity < 1000:
+    print("Evasive maneuvers required")
+else:
+    print("Object poses no threat")
