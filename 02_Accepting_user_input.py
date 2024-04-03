9bd8190d88d8d95d6cca14bedf11c848096472d4
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-04-02 20:58:40 -0500

    Accepting user input

---
+++ 02_Accepting_user_input.py
@@ -0,0 +1,5 @@
+# Accepting user input
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyear = parsecs * 3.26
+print(parsecs_input + " parsecs is " + str(lightyear) + " lightyears")
