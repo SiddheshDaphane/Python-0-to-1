54eceb611b0773e431f78c69b0d501d253e69d72
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-04-08 16:50:32 -0500

    Strings in python

--- 05_strings.py
+++ 05_strings.py
@@ -107,3 +107,35 @@ print(sentences)
 for i in sentences:
     if 'temperature' in i:
         print(i)
+
+    
+# f strings. 
+mass_percentage = "1/6"
+print(f"On the Moon, you would weigh about {mass_percentage} of your weight on Earth.")
+
+
+# ROUND function
+
+print(round(100/6, 1))
+print(f"On the Moon, you would weigh about {round(100/6, 1)}% of your weight on Earth.")
+
+# title()
+
+subject = "interesting facts about the moon"
+heading = f"{subject.title()}"
+print(heading)
+
+
+name = 'Ganymede'
+planet = 'Mars'
+gravity = '1.43'
+
+template = """Gravity Facts about {name}
+----------------------------------------
+Planet Name: {planet}
+Gravity on {name}: {gravity} m/s2"""
+
+template = """Gravity Facts about {name}
+----------------------------------------
+Planet Name: {planet}
+Gravity on {name}: {gravity} m/s2"""
