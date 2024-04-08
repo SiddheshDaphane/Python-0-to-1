b7a7cd603830b946be0a410dd8db8b78408131b3
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-04-08 16:35:56 -0500

    Strings in python

--- 05_strings.py
+++ 05_strings.py
@@ -97,3 +97,13 @@ print("saturn" in text.lower())
 
 moon_facts = ["The moon is drifting away from the Earth.", "On avg, the Moon is moving about 4cm every year."]
 print('      '.join(moon_facts))
+
+
+text = "Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth. On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."
+
+sentences = text.split('. ')
+print(sentences)
+
+for i in sentences:
+    if 'temperature' in i:
+        print(i)
