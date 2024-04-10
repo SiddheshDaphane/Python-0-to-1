51ffce0de52ee5d72d5eabde84b9ecdebe34edcb
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-04-10 13:20:11 -0500

    Arithmetic operators

--- 06_math_operations/02_Arithmetic_operators.py
+++ 06_math_operations/02_Arithmetic_operators.py
@@ -34,3 +34,14 @@ from math import ceil, floor
 
 print(ceil(12.5))
 print(floor(12.5))
+
+# Read the values from the user
+
+first_planet_input = input('Enter the distnace from the sun for the first planet in km ')
+second_planet_input = input('Enter the distance from the sun for the second planet in km ')
+
+first_planet = int(first_planet_input)
+second_planet = int(second_planet_input)
+
+distance_km1 = second_planet - first_planet
+print(abs(distance_km1))
