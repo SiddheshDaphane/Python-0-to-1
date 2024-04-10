0f97ee4c371b2d3a273110d59787e827f64a0c13
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-04-10 12:04:45 -0500

    Math operations

---
+++ 06_math_operations.py
@@ -0,0 +1,22 @@
+seconds = 1042
+display_min = 1042 // 60 
+display_seconds = 1042 % 60 # remainder 
+
+print(display_min)
+print(display_seconds)
+
+# Order of operation
+
+# 1) Parentheses
+# 2) Exponents
+# 3) Multiplication and division
+# 4) Addition and subtraction
+
+result_01 = 1032 + 26 * 2
+print(result_01)
+
+result_02 = 1032 + (26 * 2)
+print(result_02)
+
+result_03 = (1032 + 26) * 2
+print(result_03)
