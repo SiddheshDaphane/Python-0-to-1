7419e7d05a709235a761847f7c8be14ced120cd1
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-04-10 12:33:57 -0500

    Arithmetic operators

--- 06_math_operations/02_Arithmetic_operators.py
+++ 06_math_operations/02_Arithmetic_operators.py
@@ -6,3 +6,31 @@ print(distance_km)
 
 distance_mi = distance_km / 1.609344
 print(distance_mi)
+
+
+# Convert strings to numbers
+
+demo_int = int('215')
+print(demo_int)
+
+demo_float = float('215.3')
+print(demo_float)
+
+
+# Absolute values
+print(32-16)
+print(abs(16-32))
+
+
+# Rounding
+
+print(round(1.4))
+print(round(1.454545,3))
+
+
+# CEIL() and FLOOR()
+
+from math import ceil, floor
+
+print(ceil(12.5))
+print(floor(12.5))
