6680236866eb7b7f5c49b378e10e66badbef1a71
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-04-12 10:09:25 -0500

    Lists

---
+++ 07_Lists/01_intro_to_list.py
@@ -0,0 +1,41 @@
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+# Access list items by index
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+print("The first planet is", planets[0])
+print("The second planet is", planets[1])
+print("The third planet is", planets[2])
+
+# Because all indexes start from 0, [1] is the second item, [2] is the third, and so forth.
+
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets[3] = "Red Planet"
+print("Mars is also known as", planets[3])
+
+# Determine the length of a list
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+number_of_planets = len(planets)
+print("There are", number_of_planets, "planets in the solar system.")
+
+# Add values in the list
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets.append("Pluto")
+number_of_planets = len(planets)
+print("There are actually", number_of_planets, "planets in the solar system.")
+
+# Remove values from lists
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
+planets.pop()  # Goodbye, Pluto
+number_of_planets = len(planets)
+print("No, there are definitely", number_of_planets, "planets in the solar system.")
+
+# Use negative indexes
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+print("The last planet is", planets[-1])
+print("The penultimate planet is", planets[-2])
+
+# Find a value in a list
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+jupiter_index = planets.index("Jupiter")
+print("Jupiter is the", jupiter_index + 1, "planet from the sun")
